b26a52ae0ec53a10237a308d5bc10813e0fc5881
Rao Atif <70482315+Atifba86@users.noreply.github.com>
2021-09-10 23:57:47 +0500

    Add files via upload

---
+++ MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using website.Models;
+
+namespace website.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ViewResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "EndGame"
+            };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        //movies
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+
+        }
+        
+
+
+    }
+}
